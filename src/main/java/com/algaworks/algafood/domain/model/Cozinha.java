4b6c58066f382a2a0292e7aaa999df8a45a207e1
josejunior <joseademarti@gmail.com>
2021-04-26 12:10:56 -0300

    Refatorado os repositorios para o JPARepository

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,23 +1,29 @@
 package com.algaworks.algafood.domain.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonRootName;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-@Entity
+@JsonRootName("cozinha")
 @Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Cozinha {
+
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@EqualsAndHashCode.Include
-	Long id;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	
-	
+	@Column(nullable = false)
 	private String nome;
-
+	
 }
