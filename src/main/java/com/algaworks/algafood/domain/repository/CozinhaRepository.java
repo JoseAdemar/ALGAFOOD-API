a5585d8afa7e7e02008619b184836d6bcba4c5d0
josejunior <joseademarti@gmail.com>
2021-04-27 12:54:21 -0300

    Implementando queryMethods

--- src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafood.domain.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,8 @@ import com.algaworks.algafood.domain.model.Cozinha;
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
 
-//	List<Cozinha> consultarPorNome(String nome);
+  Optional<Cozinha> findByNome(String nome); //metodo listar por nome
+  //Optional<Cozinha> findNomeByNome(String nome); //metodo listar por nome
+  //List<Cozinha> nome (String nome); metodo listar por nome
 	
 }
