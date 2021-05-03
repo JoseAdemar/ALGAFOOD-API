e6347c8242a244a9e4d0cdf245042c5353809aa7
josejunior <joseademarti@gmail.com>
2021-05-03 11:35:11 -0300

    Criando uma fábrica de Specifications

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -1,14 +1,16 @@
 package com.algaworks.algafood.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 
 @Repository
-public interface RestauranteRepository extends JpaRepository<Restaurante, Long> , RestauranteRepositoryQueries{
+public interface RestauranteRepository extends JpaRepository<Restaurante, Long> 
+, RestauranteRepositoryQueries,JpaSpecificationExecutor<Restaurante>{
 
-//	//List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal); //pesquisa entre dois valores
+  // List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal); //pesquisa entre dois valores
 //	
 //	List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);//pesquisa onde contem um nome e também um id
 //	
--- src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,15 +1,15 @@
 package com.algaworks.algafood.infrastructure.repository;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepositoryQueries;
@@ -70,19 +70,69 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 //	}
 	
 	
-	//consulta simples com criteriaQuery
+	//consulta  com criteriaQuery
+	
+//	@Override
+//	public List<Restaurante> find(String nome, 
+//			BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+//		CriteriaBuilder builder = manager.getCriteriaBuilder();
+//		
+//		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
+//		Root<Restaurante> root = criteria.from(Restaurante.class);
+//
+//		Predicate nomePredicate = builder.like(root.get("nome"), "%" + nome + "%");
+//		
+//		Predicate taxaInicialPredicate = builder
+//				.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial);
+//		
+//		Predicate taxaFinalPredicate = builder
+//				.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal);
+//		
+//		criteria.where(nomePredicate, taxaInicialPredicate, taxaFinalPredicate);
+//		
+//		TypedQuery<Restaurante> query = manager.createQuery(criteria);
+//		return query.getResultList();
+//	}
+//	
+//}
+	
+	//Consulta dinamica com criteriQuery
 	
 	@Override
 	public List<Restaurante> find(String nome, 
 			BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
-		CriteriaBuilder builder = manager.getCriteriaBuilder();
+		var builder = manager.getCriteriaBuilder();
 		
-		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
-		criteria.from(Restaurante.class);
+		var criteria = builder.createQuery(Restaurante.class);
+		var root = criteria.from(Restaurante.class);
+
+		var predicates = new ArrayList<Predicate>();
 		
-		TypedQuery<Restaurante> query = manager.createQuery(criteria);
+		if (StringUtils.hasText(nome)) {
+			predicates.add(builder.like(root.get("nome"), "%" + nome + "%"));
+		}
+		
+		if (taxaFreteInicial != null) {
+			predicates.add(builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial));
+		}
+		
+		if (taxaFreteFinal != null) {
+			predicates.add(builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal));
+		}
+		
+		criteria.where(predicates.toArray(new Predicate[0]));
+		
+		var query = manager.createQuery(criteria);
 		return query.getResultList();
 	}
 	
 }
+	
+	
+	
+	
+	
+	
+	
+	
 	
