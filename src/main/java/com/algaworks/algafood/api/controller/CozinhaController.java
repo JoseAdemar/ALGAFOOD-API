72256d9455ff8d380ddd3c590ce8b27a55d3f30b
Jose Ademar De Oliveira Junior <jose.oliveira@palmares.gov.br>
2021-05-25 13:06:31 -0300

    Melhorando as respostas http

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
@@ -76,8 +77,8 @@ public class CozinhaController {
 			cadastroCozinha.excluir(cozinhaId);	
 			return ResponseEntity.noContent().build();
 			
-		} catch (EntidadeNaoEncontradaException e) {
-			return ResponseEntity.notFound().build();
+	} catch (EntidadeNaoEncontradaException e) {
+		    throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
 			
 		} catch (EntidadeEmUsoException e) {
 			return ResponseEntity.status(HttpStatus.CONFLICT)
